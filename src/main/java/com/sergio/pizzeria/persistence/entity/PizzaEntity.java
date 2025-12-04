60578211a5b71646aa79bac7d28ec7db43d080a9
Sergio Guzmán <serguz95444@gmail.com>
2025-12-04 11:12:10 -0600

    Agregar entidad AuditableEntity y modificar PizzaEntity para incluir auditoría de fechas

--- src/main/java/com/sergio/pizzeria/persistence/entity/PizzaEntity.java
+++ src/main/java/com/sergio/pizzeria/persistence/entity/PizzaEntity.java
@@ -1,39 +1,52 @@
 package com.sergio.pizzeria.persistence.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_pizza", nullable = false)
-    private Integer idPizza;
+public class PizzaEntity extends AuditableEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pizza", nullable = false)
+	private Integer idPizza;
 
-    @Column(nullable = false, length = 30, unique = true)
-    private String name;
+	@Column(nullable = false, length = 30, unique = true)
+	private String name;
 
-    @Column(nullable = false, length = 150)
-    private String description;
+	@Column(nullable = false, length = 150)
+	private String description;
 
-    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
-    private Double price;
+	@Column(nullable = false, columnDefinition = "Decimal(5,2)")
+	private Double price;
 
-    @Column(columnDefinition = "TINYINT")
-    private Boolean vegetarian;
+	@Column(columnDefinition = "TINYINT")
+	private Boolean vegetarian;
 
-    @Column(columnDefinition = "TINYINT")
-    private Boolean vegan;
+	@Column(columnDefinition = "TINYINT")
+	private Boolean vegan;
 
-    @Column(columnDefinition = "TINYINT", nullable = false)
-    private Boolean available;
+	@Column(columnDefinition = "TINYINT", nullable = false)
+	private Boolean available;
+
+	@Column(name = "created_date")
+	@CreatedDate
+	private LocalDateTime createdDate;
+
+	@Column(name = "modified_date")
+	@LastModifiedDate
+	private LocalDateTime modifiedDate;
 
 }
-
